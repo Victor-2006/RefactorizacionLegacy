c7778ada9cdf019c7811e46ac6af408147b7cd9a
Victor-2006 <victorsanchez.25@campuscamara.es>
2026-05-18 13:28:58 +0200

    Sustituir if-else por if-return

--- src/FacturacionLegacy.java
+++ src/FacturacionLegacy.java
@@ -7,22 +7,22 @@ public class FacturacionLegacy {
     private static final double DESCUENTO_ESTANDAR = 0.15;
     private static final double DESCUENTO_CLIENTE_TIPO2 = 0.05;
     // Método a refactorizar
-    public double calcularTotal(double importe, int tipoCliente, boolean clienteVip) {
-        if (importe > 0) {
-            if (tipoCliente == 1) {
-                if (clienteVip == true)
-                    return importe - (importe * DESCUENTO_VIP);
-                else
-                    return importe - (importe * DESCUENTO_ESTANDAR);
-            } else {
-                if (tipoCliente == 2) {
-                    return importe - (importe * DESCUENTO_CLIENTE_TIPO2);
-                } else {
-                    return importe;
-                }
-            }
-        } else {
+    public double calcularTotal(double importeBase, int tipoCliente, boolean esSocioVip) {
+        if (importeBase <= 0) {
             return 0;
         }
-    }
+        if (tipoCliente == 1) {
+            
+            if (esSocioVip) {
+                return importeBase - (importeBase * DESCUENTO_VIP);
+                        }
+
+                return importeBase - (importeBase * DESCUENTO_ESTANDAR);
+            }
+     
+            if (tipoCliente == 2) {
+                    return importeBase - (importeBase * DESCUENTO_CLIENTE_TIPO2);
+                    }
+                    return importeBase;
+ 
 }
