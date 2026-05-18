c4ffffee2a754f4dbffdae6403d00de778cc5c5d
Victor-2006 <victorsanchez.25@campuscamara.es>
2026-05-18 14:18:56 +0200

    Refactorizado el calcular total

--- src/FacturacionLegacy.java
+++ src/FacturacionLegacy.java
@@ -15,22 +15,17 @@ public class FacturacionLegacy {
     private static final double DESCUENTO_CLIENTE_TIPO2 = 0.05;
 
     public double calculaTotal(double importeBase, int tipoCliente, boolean esSocioVip) {
-        return calcularTotal(importeBase, tipoCliente, esSocioVip);
-    }
-    // Método a refactorizar
-    public double calcularTotal(double importeBase, int tipoCliente, boolean esSocioVip) {
+        // Método a refactorizar
         if (importeBase <= 0) {
             return 0;
         }
         if (tipoCliente == 1) {
-
             if (esSocioVip) {
                 return importeBase - (importeBase * DESCUENTO_VIP);
                         }
 
                 return importeBase - (importeBase * DESCUENTO_ESTANDAR);
             }
-     
             if (tipoCliente == 2) {
                     return importeBase - (importeBase * DESCUENTO_CLIENTE_TIPO2);
                     }
