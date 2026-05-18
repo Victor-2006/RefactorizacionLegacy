8b29257327d686a0101df8257b7e065fc9cfff7a
Victor-2006 <victorsanchez.25@campuscamara.es>
2026-05-18 13:21:13 +0200

    Eliminacion de numeros magicos

--- src/FacturacionLegacy.java
+++ src/FacturacionLegacy.java
@@ -3,18 +3,20 @@
  * ADVERTENCIA: Código con alta deuda técnica. No modificar la firma del método.
  */
 public class FacturacionLegacy {
-
+    private static final double DESCUENTO_VIP = 0.25;
+    private static final double DESCUENTO_ESTANDAR = 0.15;
+    private static final double DESCUENTO_CLIENTE_TIPO2 = 0.05;
     // Método a refactorizar
     public double calcularTotal(double importe, int tipoCliente, boolean clienteVip) {
         if (importe > 0) {
             if (tipoCliente == 1) {
                 if (clienteVip == true)
-                    return importe - (importe * 0.25);
+                    return importe - (importe * DESCUENTO_VIP);
                 else
-                    return importe - (importe * 0.15);
+                    return importe - (importe * DESCUENTO_ESTANDAR);
             } else {
                 if (tipoCliente == 2) {
-                    return importe - (importe * 0.05);
+                    return importe - (importe * DESCUENTO_CLIENTE_TIPO2);
                 } else {
                     return importe;
                 }
